85bcca595f603d43f86d75d65393a128921fc71a
Jingxuan Tu <jxtu@brandeis.edu>
2018-04-27 22:54:35 -0400

    replace

--- playground.py
+++
@@ -1,6 +0,0 @@
-from urllib import request
-
-link = 'https://charts-static.billboard.com/img/2003/12/michael-jackson-9to-number-ones-f42-174x174.jpg'
-a = request.urlretrieve(link, '111')
-print(a)
-print(244 * 110)
